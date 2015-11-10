4d0f7aaa7bbac45dc61af378651497d27fc554d7
Wei Dong <wdong@wdong.org>
2015-11-10 15:27:35 -0500

    fixed glog

--- caffex.cpp
+++ caffex.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <fstream>
 #include <algorithm>
+#define CAFFEX_IMPL 1
 #include "caffex.h"
 
 namespace caffex {
@@ -31,8 +33,9 @@ Caffex::Caffex(string const& model_dir, unsigned batch)
 
 
     // set mean file
-    {
-        string mean_file = model_dir + "/caffe.mean";
+    string mean_file = model_dir + "/caffe.mean";
+    std::ifstream test(mean_file.c_str());
+    if (test) {
         BlobProto blob_proto;
         ReadProtoFromBinaryFileOrDie(mean_file.c_str(), &blob_proto);
 
@@ -57,6 +60,11 @@ Caffex::Caffex(string const& model_dir, unsigned batch)
         cv::merge(channels, merged);
         cv::Scalar channel_mean = cv::mean(merged);
         mean = cv::Mat(input_height, input_width, merged.type(), channel_mean);
+    }
+    else {
+        std::cerr << "Failed to load mean file.";
+        mean = cv::Mat(input_height, input_width, CV_32FC3);
+        mean = cv::Scalar(0);
     }
 
     {
